a9aae8ae62b0d822fd7ec01659b925ff5556ef43
Aditya sinha <adityakumar113141@gmail.com>
2021-03-02 02:04:09 +0530

    Creating Activity from Vue Step 4 Completed

--- activities/Pawn.activity/js/activity.js
+++ activities/Pawn.activity/js/activity.js
@@ -28,5 +28,26 @@ var app = new Vue({
 			this.pawns.push(this.currentenv.user.colorvalue);
 			this.displayText = this.currentenv.user.name + " played";
 		},
+
+		onStop: function () {
+			// Save current pawns in Journal on Stop
+			var context = {
+				pawns: this.pawns
+			};
+			this.$refs.SugarJournal.saveData(context);
+		},
+
+		onJournalNewInstance: function() {
+			console.log("New instance");
+		},
+		
+		onJournalDataLoaded: function (data, metadata) {
+			console.log("Existing instance");
+			this.pawns = data.pawns;
+		},
+		
+		onJournalLoadError: function(error) {
+			console.log("Error loading from journal");
+		},
 	}
 });
