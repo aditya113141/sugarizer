50b88c1f062b97d3102ca0b476b3e5e98616bdbf
Aditya sinha <adityakumar113141@gmail.com>
2021-03-01 23:35:03 +0530

    Creating Activity from Vue Step 2 Completed

--- activities/Pawn.activity/js/activity.js
+++ activities/Pawn.activity/js/activity.js
@@ -9,6 +9,14 @@ requirejs.config({
 // Vue main app
 var app = new Vue({
 	el: '#app',
-	data: {},
-	methods: {}
+	data: {
+		displayText: ''
+	},
+	methods: {
+		initialized: function () {
+			// Sugarizer initialized
+			var environment = this.$refs.SugarActivity.getEnvironment();
+			this.displayText = "Hello " + environment.user.name + "!";	
+		}
+	}
 });
